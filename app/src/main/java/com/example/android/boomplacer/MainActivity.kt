6bb313eba4092278ebbd403ac871f00f7f761847
MaxNF <moneyseller@mail.ru>
2019-12-07 05:01:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/boomplacer/MainActivity.kt
@@ -0,0 +1,12 @@
+Ppackage com.example.android.boomplacer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
